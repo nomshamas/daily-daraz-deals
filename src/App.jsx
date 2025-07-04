0afb8b82c8d6469f9d6c0cf39fb376c3152af8f6
nomshamas <nomshamas@gmail.com>
2025-07-04 11:44:53 +0500

    remove unnecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
+import IndexPage from './pages/index'
 function App() {
-  return (
-    <main className="text-center p-6">
-      <h1 className="text-2xl font-bold">Daily Daraz Deals 🚀</h1>
-      <p className="text-gray-500">React + Vite + Tailwind setup is ready</p>
-    </main>
-  )
+  return <IndexPage />
 }
-
 export default App
