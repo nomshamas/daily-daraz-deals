ccdf8fd5d5e3dda1c3cdc4228ae28ab2bbab0007
nomshamas <nomshamas@gmail.com>
2025-07-05 15:48:17 +0500

    deploy project after build and title change

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/daily-daraz-deals/', // 👈 repo name
   plugins: [
     react(),
     tailwindcss(),
